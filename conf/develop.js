c160f66ce1468457d20aaa91864a248228bed341
ningchunlei <ningchunlei@gmail.com>
2012-11-05 15:31:30 +0800

    init

--- conf/develop.js
+++ conf/develop.js
@@ -1,3 +1,4 @@
 exports.server = {"port":8090,"topN":100}
 exports.redis = {"port": 6379,"ip":"127.0.0.1","passwd":""}
 exports.kestrel = {"port": 11213,"ip":"127.0.0.1"}
+exports.db = {"port": 6379,"ip":"127.0.0.1"}
--- prelationservice.js
+++ prelationservice.js
@@ -7,6 +7,7 @@ var util = require("util");
 var memcache = require('memcache');
 var buffer = require("buffer")
 
+var IKaoDBService = require("./thrift/IKaoDBIFace")
 var RelationService = require("./thrift/PRelationIFace")
 var ShareStruct_ttypes = require("./thrift/ShareStruct_Types")
 var ErrorNo_ttypes = require("./thrift/ErrorNo_Types")
@@ -39,8 +40,23 @@ var kestrelPool = poolModule.Pool({
    log : true
 });
 
+var ikaodbPool = poolModule.Pool({
+    name : "ikaodb",
+    create   : function(callback) {
+        var dbconnection = thrift.createConnection(process.conf.db.ip, process.conf.db.port)
+        var client = thrift.createClient(IKaoDBService, dbconnection);
+        callback(null, client);
+    },
+    destroy  : function(client) { client.quit(); }, //当超时则释放连接
+    max      : 10,   //最大连接数
+    idleTimeoutMillis : 10,  //超时时间
+    log : true
+})
+
+
 var FALSE = -1
 var TRUE = 1
+var REDISERROR = -2
 
 var FOLLOWED = 2
 var FOLLOWSTR = "_Follow_"
@@ -54,24 +70,31 @@ var server = exports.relation = thrift.createServer(RelationService,{
             if(ret == TRUE){
                 response(FOLLOWED)
                 return;
+            }else if(ret == REDISERROR){
+                response(REDISERROR)
+                return
             }
-            pool.borrow(function(err,client){
-                var uidKey = uid + FOLLOWSTR+type;
-                client.zadd(uidKey,new Date().getTime(),followId,function(err,reply){
-                    pool.release(client);
-                    if(reply!=1){
-                        response(FALSE)
-                        return
-                    }
-                    response(TRUE)
-                    addFans(followId,uid,type,function(reply){
-                        if(reply!=TRUE){
-                            process.log.error(util.format("addFollow:uid=%s,followId=%s,type=%d,err=to addFans",uid,followId,type))
-                            kestrelPool.borrow(function(err,client){
-                                client.set("AddFollow",'{"uid":uid,"followId":followId,"type":type}',function(){kestrelPool.release(client)})
+            ikaodbPool.borrow(function(err,idb){
+                idb.addFollow(uid,followId,type,function(err,reply){
+                    ikaodbPool.release(idb);
+                    if(reply==TRUE){
+                        pool.borrow(function(err,client){
+                            var uidKey = uid + FOLLOWSTR+type;
+                            client.zadd(uidKey,new Date().getTime(),followId,function(err,reply){
+                                pool.release(client);
+                                if(reply!=1){
+                                    response(FALSE)
+                                    return
+                                }
+                                response(TRUE)
+                                kestrelPool.borrow(function(err,client){
+                                    client.set("AddFollow",'{"uid":uid,"followId":followId,"type":type}',function(){kestrelPool.release(client)})
+                                })
                             })
-                        }
-                    })
+                        })
+                    }else{
+                        response(FALSE);
+                    }
                 })
             })
         })
@@ -82,7 +105,9 @@ var server = exports.relation = thrift.createServer(RelationService,{
         pool.borrow(function(err,client){
             client.zrank(uid+FOLLOWSTR+type,followId,function(err,reply){
                 pool.release(client)
-                if(reply==null){
+                if(err!=null){
+                    response(REDISERROR)
+                }else if(reply==null){
                     response(FALSE)
                 }else{
                     response(TRUE);
@@ -93,65 +118,91 @@ var server = exports.relation = thrift.createServer(RelationService,{
 
     cancelFollow: function(uid,followId,type,response){
         process.log.info(util.format("cancelFollow:uid=%s,followId=%s,type=%d",uid,followId,type))
-        pool.borrow(function(err,client){
-              client.zrem(uid+FOLLOWSTR+type,followId,function(err,reply){
-                    pool.release(client);
-                    if(reply==1){
-                        response(TRUE)
-                        cancelFans(followId,uid,type,function(reply){
-                             if(reply!=TRUE){
-                                 process.log.error(util.format("cancelFollow:uid=%s,followId=%s,type=%d,err=to cancelFollow",uid,followId,type))
-                                 kestrelPool.borrow(function(err,client){
-                                     client.set("CancelFollow",'{"uid":uid,"followId":followId,"type":type}',function(){kestrelPool.release(client)})
-                                 })
-                             }
+        ikaodbPool.borrow(function(err,idb){
+            idb.cancelFollow(uid,followId,type,function(err,reply){
+                ikaodbPool.release(idb)
+                if(reply==TRUE){
+                    pool.borrow(function(err,client){
+                        client.zrem(uid+FOLLOWSTR+type,followId,function(err,reply){
+                            pool.release(client);
+                            if(reply==1){
+                                response(TRUE)
+                                kestrelPool.borrow(function(err,client){
+                                    client.set("CancelFollow",'{"uid":uid,"followId":followId,"type":type}',function(){kestrelPool.release(client)})
+                                })
+                            }else{
+                                response(FALSE)
+                            }
                         })
-                    }else{
-                        response(FALSE)
-                    }
-              })
+                    })
+                }else{
+                   response(FALSE)
+                }
+            })
         })
     },
 
     cancelFans:function(uid,fansId,type,response){
         process.log.info(util.format("cancelFans:uid=%s,fansId=%s,type=%d",uid,followId,type))
-        pool.borrow(function(err,client){
-             client.zrem(uid+FANSTR+type,fansId,function(err,reply){
-                  pool.release(client)
-                  if(reply==1){
-                      response(TRUE);
-                  }else{
-                      response(FALSE)
-                  }
+        ikaodbPool.borrow(function(err,idb){
+             idb.cancelFans(uid,fansId,type,function(err,reply){
+                 ikaodbPool.release(idb)
+                 if(reply==TRUE){
+                     pool.borrow(function(err,client){
+                         client.zrem(uid+FANSTR+type,fansId,function(err,reply){
+                             pool.release(client)
+                             if(reply==1){
+                                 response(TRUE);
+                             }else{
+                                 response(FALSE)
+                             }
+                         })
+                     })
+                 }else{
+                    response(FALSE)
+                 }
              })
         })
     },
 
     addFans: function(uid,fansId,type,response){
         process.log.info(util.format("cancelFans:uid=%s,fansId=%s,type=%d",uid,followId,type))
-        pool.borrow(function(err,client){
-             client.zadd(uid+FANSTR+type,new Date().getTime(),fansId,function(err,reply){
-                 pool.release(client)
-                 if(reply==1){
-                     response(TRUE)
-                 }else{
-                     response(FALSE)
-                 }
-             })
+        ikaodbPool.borrow(function(err,idb){
+            idb.addFans(uid,fansId,type,function(err,reply){
+                ikaodbPool.release(idb)
+                if(reply==TRUE){
+                    pool.borrow(function(err,client){
+                        client.zadd(uid+FANSTR+type,new Date().getTime(),fansId,function(err,reply){
+                            pool.release(client)
+                            if(reply==1){
+                                response(TRUE)
+                            }else{
+                                response(FALSE)
+                            }
+                        })
+                    })
+                }else{
+                    response(FALSE)
+                }
+            })
         })
     },
 
     getFollow: function(uid,start,len,type,response){
         process.log.info(util.format("getFollow:uid=%s,start=%d,len=%d,type=%d",uid,start,len,type))
         pool.borrow(function(err,client){
-             client.zrangewithscores(uid+FOLLOWSTR+type,start-1,start+len-1,function(err,reply){
+             client.zrevrangewithscores(uid+FOLLOWSTR+type,start-1,start+len-1,function(err,reply){
                  pool.release(client)
-                if(err!=null){
-                    process.log.error(util.format("getFollow:uid=%s,start=%d,len=%d,type=%d,err=%s",uid,start,len,type,err))
-                    response([])
-                    return
-                }
-                response(reply)
+                 if(err!=null){
+                       ikaodbPool.borrow(function(err,idb){
+                            idb.getFollow(uid,start,len,type,function(err,reply){
+                                ikaodbPool.release(idb);
+                                response(reply)
+                            })
+                       })
+                 }else{
+                    response(reply)
+                 }
              })
         })
     },
@@ -162,11 +213,15 @@ var server = exports.relation = thrift.createServer(RelationService,{
             client.zrangewithscores(uid+FANSTR+type,start-1,start+len-1,function(err,reply){
                 pool.release(client)
                 if(err!=null){
-                    process.log.error(util.format("getFans:uid=%s,start=%d,len=%d,type=%d,err=%s",uid,start,len,type,err))
-                    response([])
-                    return
+                    ikaodbPool.borrow(function(err,idb){
+                        idb.getFans(uid,start,len,type,function(err,reply){
+                            ikaodbPool.release(idb);
+                            response(reply)
+                        })
+                    })
+                }else{
+                    response(reply)
                 }
-                response(reply)
             })
         })
     }
